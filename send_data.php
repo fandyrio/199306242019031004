ffb59ded3708e1cd4a50ddc1ef5e8ef7ccd51112
fandyrio <fandyrio@gmail.com>
2023-07-25 10:52:15 +0700

    pertama

--- send_data.php
+++ send_data.php
@@ -1,16 +1,2 @@
 <?php
-include "controller/sendData.php";
-include "model/functionCommon.php";
-
-use controller\sendData;
-use model\functionCommon;
-
-$send_data=new sendData();
-$set_conn=$send_data->setConnection();
-
-$function_common=new functionCommon();
-$data_perdata_pk=$function_common->getDataPKPerdata($set_conn);
-
-$send_data_api=$send_data->sendDataPerdataPK($data_perdata_pk);
-echo $send_data_api;
 ?>
